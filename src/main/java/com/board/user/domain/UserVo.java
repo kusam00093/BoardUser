e389a80ca98f8f95350bec664830eeeacc787142
kusam00093 <yusanghun12@naver.com>
2024-03-22 17:23:22 +0900

    resultmap 추가

--- src/main/java/com/board/user/domain/UserVo.java
+++ src/main/java/com/board/user/domain/UserVo.java
@@ -1,16 +1,20 @@
 package com.board.user.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 // @Data : @Getter, @Setter, @ToString, @NoArgsConstruvtor, @EqualsAndHashCode, @RequiedArgsConstructor
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserVo {
 	private String userid;
 	private String passwd;
 	private String username;
 	private String email;
-	private String upoint;
+	private int    upoint;
 	private String indate;
 }
